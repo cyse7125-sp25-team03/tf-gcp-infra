e3e11e15b4c86996bb873abd334ec973d916c048
aakanksha-arun <desai.aaka@northeastern.edu>
2025-02-06 15:33:24 -0500

    add binary auth and iam creds api

--- gcp-org/project-demo.tf
+++ gcp-org/project-demo.tf
@@ -8,3 +8,13 @@ resource "google_project_service" "gke_demo" {
   service = "container.googleapis.com"
   disable_on_destroy = false
 }
+resource "google_project_service" "binary_auth_demo" {
+  project = var.demo_project_id
+  service = "binaryauthorization.googleapis.com"
+  disable_on_destroy = false
+}
+resource "google_project_service" "iamcredentials_demo" {
+  project = var.demo_project_id
+  service = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
+}
--- gcp-org/project-dev.tf
+++ gcp-org/project-dev.tf
@@ -9,3 +9,13 @@ resource "google_project_service" "gke_dev" {
   service = "container.googleapis.com"
   disable_on_destroy = false
 }
+resource "google_project_service" "binary_auth_dev" {
+  project = var.dev_project_id
+  service = "binaryauthorization.googleapis.com"
+  disable_on_destroy = false
+}
+resource "google_project_service" "iamcredentials_dev" {
+  project = var.dev_project_id
+  service = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
+}
