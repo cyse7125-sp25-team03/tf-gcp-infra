a07eb2a07389b44d791015d2b126bf724e4a0366
aakanksha-arun <desai.aaka@northeastern.edu>
2025-02-06 15:59:33 -0500

    run tf fmt

--- gcp-org/variables.tf
+++ gcp-org/variables.tf
@@ -1,17 +1,17 @@
 variable "demo_project_id" {
-  type = string
+  type        = string
   description = "Demo Project ID"
 }
 variable "dev_project_id" {
-  type = string
+  type        = string
   description = "Dev Project ID"
 }
 variable "dns_project_id" {
-  type = string
+  type        = string
   description = "DNS Project ID"
 }
 variable "region" {
-  type = string
-  default = "us-east1"
+  type        = string
+  default     = "us-east1"
   description = "Region"
 }
