696434f127aef03bca060f8bb473bc489e6bc0d7
aakanksha-arun <desai.aaka@northeastern.edu>
2025-02-06 15:38:03 -0500

    add gke and vpc in dev

---
+++ gcp-project-dev/variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+variable "project_id" {
+  type        = string
+  description = "Project ID"
+}
+variable "region" {
+  type        = string
+  description = "region"
+}
---
+++ modules/gke/variables.tf
@@ -0,0 +1,5 @@
+variable "environment" {}
+variable "project_id" {}
+variable "region" {}
+variable "gke_subnet_name" {}
+variable "network_name" {}
