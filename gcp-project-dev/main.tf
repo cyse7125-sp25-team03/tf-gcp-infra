9bc68897b128b725bc60a3c4467f1a4d39c2a479
aakanksha-arun <desai.aaka@northeastern.edu>
2025-02-06 17:59:01 -0500

    tf fmt

--- gcp-project-dev/main.tf
+++ gcp-project-dev/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source      = "../modules/vpc"
-  project_id  = var.project_id
-  environment = var.environment
-  region      = var.region
-  pod_ranges = var.pod_ranges
+  source         = "../modules/vpc"
+  project_id     = var.project_id
+  environment    = var.environment
+  region         = var.region
+  pod_ranges     = var.pod_ranges
   service_ranges = var.service_ranges
 }
 module "gke" {
@@ -15,6 +15,6 @@ module "gke" {
   network_name    = module.vpc.network_name
   machine_type    = var.machine_type
   disk_type       = var.disk_type
-  pod_ranges = var.pod_ranges
-  service_ranges = var.service_ranges
+  pod_ranges      = var.pod_ranges
+  service_ranges  = var.service_ranges
 }
