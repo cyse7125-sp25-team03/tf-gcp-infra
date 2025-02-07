a5cba483f11da937b938a84482a19e2c6110aea5
Sohan Patil <patil.sohan@northeastern.edu>
2025-02-07 04:32:21 -0500

    bastion host added

--- gcp-project-dev/main.tf
+++ gcp-project-dev/main.tf
@@ -18,3 +18,16 @@ module "gke" {
   pod_ranges      = var.pod_ranges
   service_ranges  = var.service_ranges
 }
+
+module "bastion" {
+  source               = "../modules/bastion"
+  project_id           = var.project_id
+  environment          = var.environment
+  region               = var.region
+  network_name         = module.vpc.network_name
+  public_subnet_name   = "public-subnet-${var.environment}"
+  bastion_machine_type = var.bastion_machine_type
+  bastion_disk_size    = var.bastion_disk_size
+  bastion_disk_type    = var.bastion_disk_type
+  startup_script_path  = var.startup_script_path
+}
