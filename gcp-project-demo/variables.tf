5590f71a8573ffb1739ba60dc0b72efbdc07e851
aakanksha-arun <desai.aaka@northeastern.edu>
2025-02-07 11:55:40 -0500

    add demo env code

---
+++ gcp-project-demo/variables.tf
@@ -0,0 +1,27 @@
+variable "environment" {
+  type    = string
+}
+variable "project_id" {
+  type        = string
+  description = "Project ID"
+}
+variable "region" {
+  type        = string
+  description = "region"
+}
+variable "machine_type" {
+  type        = string
+  description = "Machine Type for the cluster"
+}
+variable "disk_type" {
+  type        = string
+  description = "Disk Type for the cluster"
+}
+variable "pod_ranges" {
+  type        = string
+  description = "Pod Ranges IP Allocation"
+}
+variable "service_ranges" {
+  type        = string
+  description = "Service Ranges IP Allocation"
+}
