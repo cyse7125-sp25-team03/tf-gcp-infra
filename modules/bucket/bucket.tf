975caa5bde7923136ac751116f377d75ee9140f8
aakanksha-arun <desai.aaka@northeastern.edu>
2025-03-14 12:14:44 -0400

    chore: run terraform fmt

--- modules/bucket/bucket.tf
+++ modules/bucket/bucket.tf
@@ -4,11 +4,11 @@ resource "google_storage_bucket" "trace-bucket" {
   force_destroy = true
 
   uniform_bucket_level_access = true
-public_access_prevention = "enforced"
+  public_access_prevention    = "enforced"
 }
 //creates empty folder in the bucket
 resource "google_storage_bucket_object" "uploads_folder" {
-  name   = "uploads/"
-  bucket = google_storage_bucket.trace-bucket.name
+  name    = "uploads/"
+  bucket  = google_storage_bucket.trace-bucket.name
   content = " "
 }
