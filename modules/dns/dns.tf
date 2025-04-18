018bd8e686c4694a55e94c35ef42842b2f738af5
aakanksha-arun <desai.aaka@northeastern.edu>
2025-04-18 11:43:31 -0400

    fix: add A record for asktrace

--- modules/dns/dns.tf
+++ modules/dns/dns.tf
@@ -3,17 +3,25 @@ data "google_dns_managed_zone" "selected_zone" {
   name = lookup(var.managed_zones, var.environment, var.managed_zones["demo"])
 }
 
-resource "google_compute_address" "api_static_ip" {
-  name   = "${var.environment}-api-static-ip"
+resource "google_compute_address" "server_static_ip" {
+  name   = "${var.environment}-server-static-ip"
   region = var.region
 
 }
 
-resource "google_dns_record_set" "dev_api_dns" {
+resource "google_dns_record_set" "api_dns" {
   name         = "api-server.${var.domains[var.environment]}"
   managed_zone = data.google_dns_managed_zone.selected_zone.name
   type         = "A"
   ttl          = 300
-  rrdatas      = [google_compute_address.api_static_ip.address]
+  rrdatas      = [google_compute_address.server_static_ip.address]
+}
+
+resource "google_dns_record_set" "trace_llm_dns" {
+  name         = "asktrace.${var.domains[var.environment]}"
+  managed_zone = data.google_dns_managed_zone.selected_zone.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_address.server_static_ip.address]
 }
 
